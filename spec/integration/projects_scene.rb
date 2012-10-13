8cceea6792ee6003852cd356b7d8982861cdee02
joerg.rothbarth <joerg.rothbarth@gmail.com>
2012-10-13 02:42:13 +0200

    adds hover stuff

--- spec/integration/projects_scene.rb
+++ spec/integration/projects_scene.rb
@@ -2,8 +2,21 @@
 require "integration/spec_helper"
 
 describe "/projects/index" do
-  it "root" do
+  it "from root to projects" do
     visit "/"
     page.should have_content('Alle noch offenen Übersetzungen')
+    click_link("projects")
+
+    # page.should be_on_url
+  end
+
+  it "from root to show project" do
+    visit "/"
+    page.should have_content('Alle noch offenen Übersetzungen')
+    click_link("projects")
+    fill_in "project[name]", :with => "Test Projekt"
+    click_button("add_project")
+    click_link("show_project")
+    page.body.should_not have_content("Internal Server Error")
   end
 end
--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -9,4 +9,7 @@ require "webmock/rspec"
 
 WebMock.allow_net_connect!
 
+#available drivers: :rack_test, :selenium, :webkit, :webkit_debug
 Capybara.default_driver = :webkit
+
+# rake db:test:prepare
